170f2cfa4993d044d59d498b113898bbede41156
Dor Kleiman <configurator@gmail.com>
2019-11-20 19:25:17 +0200

    Boilerplate

---
+++ .storybook/config.js
@@ -0,0 +1,7 @@
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+addDecorator(withKnobs);
+
+// automatically import all files ending in *.stories.js
+configure(require.context('..', true, /\.story\.jsx?$/), module);
